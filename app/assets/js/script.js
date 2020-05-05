5448a68fd1ea47f716f103d9702052f63588491c
Alexis Ducoulombier <alexisducoulombier@hotmail.fr>
2020-05-05 12:22:09 +0200

    erreur

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,10 +1,17 @@
-$( document ).ready(function() {
+
     console.log('Ready to load the file');
-  // $.ajax({
-  // })
-  // .done(function( msg ) {
-  // })
+   $.ajax({
+     type : "GET",
+     url :  "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=prenoms-a-rennes&facet=annaiss&facet=sexe&facet=prn",
+     datatype : "json",
+    
+  })
+  .done((data) => {
+    console.log(data)
+   // $("#test").html(data)
+   document.getElementById('test').innerHTML("pouet")
+
+       })
   // .fail(function( jqXHR, textStatus ) {
-  // });
-})  
+  // }); 
 
