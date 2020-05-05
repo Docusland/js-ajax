f839cc22fc47557ab270874c53b274c0ff992866
Benoît Meisso <benoit.meissonnier@students.campus.academy>
2020-05-05 16:37:45 +0200

    tentative de récupration du tableau de communes

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,10 +1,19 @@
-$( document ).ready(function() {
+tabCommune = $( document ).ready(function() {
     console.log('Ready to load the file');
-  // $.ajax({
-  // })
-  // .done(function( msg ) {
-  // })
-  // .fail(function( jqXHR, textStatus ) {
-  // });
-})  
+   $.ajax({
+     type : 'get',
+     url : '../../app/docs/laposte_hexasmal.csv',
+     dataType : 'text'
+   })
+   .done(function( data ) {
+     console.log(data);
+     $("#results").html(JSON.stringify(data))
+   })
+   .fail(function( jqXHR, textStatus ) {
+     console.log(jqXHR);
+     console.log(textStatus);
+   });
+})
+for (tabCommune  as value)
+
 
