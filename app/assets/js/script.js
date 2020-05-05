9fbb0f9025a25f5b11e00eb3c1f58e11b27a74e7
Marin <marinplt@gmail.com>
2020-05-05 16:27:23 +0200

    c'est vraiment moche et ça marche pas :(

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,10 +1,24 @@
-$( document ).ready(function() {
-    console.log('Ready to load the file');
-  // $.ajax({
-  // })
-  // .done(function( msg ) {
-  // })
-  // .fail(function( jqXHR, textStatus ) {
-  // });
-})  
-
+$(document).ready(function () {
+  console.log('Ready to load the file');
+  $.ajax({
+    type: "GET",
+    url: "../docs/laposte_hexasmal.csv",
+    datatype: "text"
+  })
+    .done(function (data) {
+      
+      function splitString(string, separator) {
+        string.split(separator);
+      }
+      var tempestString = data;
+      var tableau = splitString(tempestString, ";");
+      console.log(tableau)
+      $("#results").html(tableau)
+    })
+    .fail(function (jqXHR, textStatus, errorThrow) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrow);
+    });
+    
+})
