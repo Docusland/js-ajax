183ec68fc0b0a6bf3ba674b57aa4ee10b33a62a7
ludovic <ludovic.angenard@orange.fr>
2020-05-05 16:27:40 +0200

    Tentative d'intégrer du ajax

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,10 +1,21 @@
 $( document ).ready(function() {
     console.log('Ready to load the file');
-  // $.ajax({
-  // })
-  // .done(function( msg ) {
-  // })
-  // .fail(function( jqXHR, textStatus ) {
-  // });
+    $.ajax({
+      type : "GET",
+      url :  '../docs/laposte_hexasmal.csv ',
+      datatype : "text"
+   })
+   // cela fonctionnait bien avec les prénoms de la base de données de Rennes mais une erreur s'affiche sur le fichier
+   .done( function (data) {
+    var tableau = data.split(";")
+    
+     $("#test").html(JSON.stringify(tableau))
+    
+   })
+   .fail(function( jqXHR, textStatus,errorThrow ) {
+     console.log(jqXHR);
+     console.log(textStatus);
+     console.log(errorThrow)
+   });   
 })  
 
