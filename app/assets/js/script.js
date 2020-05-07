40fc9191b9d737ab1817857abdc2ee6fab742eda
ludovic <ludovic.angenard@orange.fr>
2020-05-07 03:26:20 +0200

    Affichage du tableau dans le html

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -2,15 +2,21 @@ $( document ).ready(function() {
     console.log('Ready to load the file');
     $.ajax({
       type : "GET",
-      url :  '../docs/laposte_hexasmal.csv ',
-      datatype : "text"
+      url :  'docs/laposte_hexasmal.csv ',
+      datatype : "text",
    })
    // cela fonctionnait bien avec les prénoms de la base de données de Rennes mais une erreur s'affiche sur le fichier
    .done( function (data) {
-    var tableau = data.split(";")
+    var tableauDesVilles = data.split("\n");
+    var ville="";
+   
+    for (let i = 0; i < tableauDesVilles.length; i++) {
+      ville=ville+"<br>"+tableauDesVilles[i]
+       
+     }
+
     
-     $("#test").html(JSON.stringify(tableau))
-    
+     $("#test").html(ville)
    })
    .fail(function( jqXHR, textStatus,errorThrow ) {
      console.log(jqXHR);
