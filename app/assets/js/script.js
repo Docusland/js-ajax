e5a5ee915c386c084a88d8d456155edda1865e65
ludovic <ludovic.angenard@orange.fr>
2020-05-08 02:46:47 +0200

    Affichage du tableau réussi

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,45 +1,66 @@
 let tableauDesVilles = [];
-$( document ).ready(function() {
-    console.log('Ready to load the file');
-    
-    $.ajax({
-      type : "GET",
-      url :  'docs/laposte_hexasmal.csv ',
-      datatype : "text",
-   })
-   // cela fonctionnait bien avec les prénoms de la base de données de Rennes mais une erreur s'affiche sur le fichier
-   .done( function (data) {
-    tableauDesVilles = data.split("\n");
-  
-   })
-   .fail(function( jqXHR, textStatus,errorThrow ) {
-     console.log(jqXHR);
-     console.log(textStatus);
-     console.log(errorThrow)
-   });   
-})
-var villeEtCode=[]
-$('#zip_code').keyup(recherche);
+let villeEtCode=[];
+let tableau=[];
+let entête='<TABLE BORDER="1"><TR><TH> Code Postale </TH><TH> Code Insee </TH><TH> Nom de la Commune </TH></TR> ';
+let html='';
+let ligne=0;
 
-const codePostale=2
-const ville=1
+const codePostale=2;
+const ville=1;
+const insee=0;
 
-function recherche()
+$( document ).ready(function() 
 {
-  if ($(this).val().length>=3){
+  console.log('Ready to load the file');
     
-  for (let i = 0; i < tableauDesVilles.length; i++) 
+  $.ajax({
+    type : "GET",
+    url :  'docs/laposte_hexasmal.csv ',
+    datatype : "text",
+  })
+  .done( function (data) 
   {
-    villeEtCode=tableauDesVilles[i].split(";")
-  
-     if ( villeEtCode[codePostale].startsWith($(this).val()))
-      {
-        console.log(villeEtCode[ville])
-      }
+    tableauDesVilles = data.split("\n");
+    for (let i = 0; i < tableauDesVilles.length; i++) 
+    {
+      villeEtCode=tableauDesVilles[i].split(";");
+      tableau.push(villeEtCode);
+    }
+  })
 
-      
-    
+  .fail(function( jqXHR, textStatus,errorThrow )
+   {
+    console.log(jqXHR);
+    console.log(textStatus);
+    console.log(errorThrow);
+  });   
+})
+
+$('#zip_code').keyup(rechercheZipCode);
+
+function rechercheZipCode()
+{
+  if ($(this).val().length>=2)
+  {
+    html='';
+    for ( ligne = 0; ligne < tableau.length; ligne++) 
+    {
+      if ( tableau[ligne][codePostale].startsWith($(this).val()))
+      {
+          
+        ajoutHtml();
+      }
+    }
+    affichageHtml();
   }
 }
+
+function ajoutHtml()
+{
+  html=html+"<TR><TD>"+tableau[ligne][codePostale]+"</TD><TD>"+tableau[ligne][insee]+"</TD><TD>"+tableau[ligne][ville]+"</TD></TR>";
 }
-   
+
+function affichageHtml()
+{
+  $('#test').html(entête+html+"</TABLE> ");
+}
